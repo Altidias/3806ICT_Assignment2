aa026093b3f9e8c96d6a3fdff2c181186a0558b5
Altidias <jacobbarany@gmail.com>
2025-05-26 12:59:45 +1000

    adjusted frontier detector header file in accordance with other code changes

--- turtlebot3_explorer/include/turtlebot3_explorer/frontier_detector.h
+++ turtlebot3_explorer/include/turtlebot3_explorer/frontier_detector.h
@@ -4,10 +4,8 @@
 #include <ros/ros.h>
 #include <nav_msgs/OccupancyGrid.h>
 #include <visualization_msgs/MarkerArray.h>
-#include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseArray.h>
 #include <vector>
-#include <queue>
 #include <mutex>
 
 struct Frontier {
@@ -18,37 +16,37 @@ struct Frontier {
 class FrontierDetector {
 public:
     FrontierDetector(ros::NodeHandle& nh, ros::NodeHandle& private_nh);
-	void run();
     ~FrontierDetector();
 
 private:
     ros::NodeHandle nh_;
     ros::Subscriber map_sub_;
-    ros::Publisher frontier_pub_;
-    ros::Publisher frontier_points_pub_;
-    ros::Timer timer_;
+    ros::Publisher frontier_pub_;         // centroids as PoseArray
+    ros::Publisher frontier_points_pub_;  // all points as PoseArray
+    ros::Publisher frontier_viz_pub_;     // visualization markers
     
     std::string map_topic_;
     std::string frontier_topic_;
     std::string frontier_points_topic_;
     int min_frontier_size_;
     double frontier_cluster_min_dist_;
-    std::vector<Frontier> last_frontiers_;
     
-    // map data
     nav_msgs::OccupancyGrid::ConstPtr map_;
-    std::mutex map_mutex_;
     
-    // detection methods
     void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& map);
-    std::vector<Frontier> detectFrontiers();
-    void clusterFrontiers(const std::vector<std::pair<int, int>>& frontier_points, std::vector<Frontier>& frontiers);
+    void detectFrontiers(std::vector<Frontier>& frontiers, 
+                        std::vector<std::pair<int, int>>& all_frontier_points);
     bool isFrontierPoint(const nav_msgs::OccupancyGrid& map, int x, int y);
-    void publishFrontiers(const std::vector<Frontier>& frontiers);
+    void clusterFrontiers(const std::vector<std::pair<int, int>>& frontier_points,
+                         std::vector<Frontier>& frontiers);
+    
+    void publishFrontierCentroids(const std::vector<Frontier>& frontiers);
+    void publishFrontierPoints(const std::vector<std::pair<int, int>>& points);
+    void publishVisualizationMarkers(const std::vector<Frontier>& frontiers,
+                                   const std::vector<std::pair<int, int>>& points);
     
     void mapToWorld(int mx, int my, double& wx, double& wy);
     bool worldToMap(double wx, double wy, int& mx, int& my);
-    void timerCallback(const ros::TimerEvent& event);
 };
 
 #endif // FRONTIER_DETECTOR_H
